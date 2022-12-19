da7edc7e2f754ab457d51c6d45802d336bf6fbfe
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-19 18:36:52 +0900

    feat: QueryClientProvider 감싸기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import {QueryClientProvider, QueryClient} from "react-query";
 import { Posts } from "./Posts";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
