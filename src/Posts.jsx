5f9276ab4cea4fe3384a64fc3e97a0f3411e3f9c
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-20 00:10:21 +0900

    feat: stale, cache 설명

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -14,9 +14,35 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery("posts", fetchPosts);
+  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts, {
+    staleTime: 2000 // 2초마다 데이터가 만료되도록 설정, default는 0초 데이터를 계속 최신으로 유지하기 위
+  });
+  // isFetching
+  // => 비동기 쿼리가 해결되지 않았음
 
-  if(!data) return <div></div>;
+  // isLoading
+  // => 가져오는 상태 쿼리함수가 아직 해결되지 않음,
+  // 캐시에 해당 데이터가 없다는 의미이기도함.(처음 부르는 쿼리)
+  // => isFetching보다 하위 집합.
+
+  if(isLoading) return <h3>Loading...</h3>;
+
+  if(isError) return (
+    <>
+      <h3>Oops, something went wrong</h3>
+      <p>{error.toString()}</p>
+    </>
+  );
+
+  //staleTime
+  // => staleTime 데이터가 유효한 최대의 시간.
+  // 데이터가 만료되지 않으면, 리페칭은 실행되지 않는다.
+
+  // cache data
+  // => 나중에 필요할 수도 있는 data
+  // 캐시가 만료되면, 가비지 컬렉션이 실행되고
+  // 클라이언트는 데이터를 사용 할 수 없게 된다.
+  // cacheTime은 default가 5분
 
   return (
     <>
