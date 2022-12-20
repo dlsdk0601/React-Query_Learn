6df391fea3ffacb144aafbfc3c33f37e710c82d5
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-21 00:47:53 +0900

    refactor: species로 연습

--- src/page/infinite/Infinite.jsx
+++ src/page/infinite/Infinite.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {InfinitePeople} from "./InfinitePeople";
+import {InfiniteSpecies} from "./InfiniteSpecies";
 import "./Infinite.css";
 
 const Infinite = () => {
   return (
     <div className="App">
       <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
+      {/*<InfinitePeople />*/}
+       <InfiniteSpecies />
     </div>
   );
 };
--- src/page/infinite/InfiniteSpecies.jsx
+++ src/page/infinite/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import {useInfiniteQuery} from "react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,41 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading, isError, error, isFetching
+  } = useInfiniteQuery(
+    "sw-species",
+    ({ pageParam = initialUrl}) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined
+      }
+    );
+
+  if(isLoading) return <div className="loading">Loading...</div>;
+
+  if(isError) return <div className="loading">Error! {error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map(page => {
+          return page.results.map((spec) => {
+            return (
+              <Species
+                key={spec.name}
+                name={spec.name}
+                language={spec.language}
+                averageLifespan={spec.average_lifespan}
+              />
+            );
+          })
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
