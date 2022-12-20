5545e0cd022ce728037ab5cdfdeb3d22507ed943
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-20 22:34:33 +0900

    feat: pagination 처리

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -3,18 +3,21 @@ import {useQuery} from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts, {
+  const { data, isLoading, isError, error } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
     staleTime: 2000 // 2초마다 데이터가 만료되도록 설정, default는 0초 데이터를 계속 최신으로 유지하기 위
   });
   // isFetching
@@ -58,11 +61,19 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+          setCurrentPage(prev => prev - 1);
+        }}>
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage(prev => prev + 1);
+          }}>
           Next page
         </button>
       </div>
