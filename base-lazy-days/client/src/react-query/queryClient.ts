8467cea2869e85d4590cb8c1992b0d4d6a403b85
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-25 23:37:54 +0900

    refactor: 기본적으로 useQuery는 onError를 제공하지 않는다. 그래서 직접 정의해서 사용해야하는데, QueryClient 인스터스에서 옵션을 추가해서 설정 가능함. 그래서 QueryClient 인스터스를 만들때, 추가

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,17 +1,24 @@
+import { createStandaloneToast } from '@chakra-ui/react';
 import { QueryClient } from 'react-query';
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+import { theme } from '../theme';
 
-// function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const title =
-//     error instanceof Error ? error.message : 'error connecting to server';
+const toast = createStandaloneToast({ theme });
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
 
-export const queryClient = new QueryClient();
+  // prevent duplicate toasts
+  toast.closeAll();
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
