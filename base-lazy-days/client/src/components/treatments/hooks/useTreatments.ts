dc3f4488f23ca7e5c576fd988e35c27c207bb42d
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-25 23:38:24 +0900

    refactor: QueryClient 에서 onError 추가 해줬으니, 작업했던 onError 삭제

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
