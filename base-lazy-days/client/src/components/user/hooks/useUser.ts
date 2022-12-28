47dc228b47bed74c8480d5ec1c6c0a4bd3900959
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-28 16:30:19 +0900

    feat: useUser hooke안에 useQuery로 user 데이터 불러오는 로직 생성

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,17 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -28,7 +30,8 @@ interface UseUser {
 
 export function useUser(): UseUser {
   // TODO: call useQuery to update user data from server
-  const user = null;
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
+  // 왼쪽 user와 오른쪽 user는 같은 변수이다. 기존의 user를 대입해서 새로운 userData를 불러오는것.
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
