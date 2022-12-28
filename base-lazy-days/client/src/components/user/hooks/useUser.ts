5e0da28771263c2ef99f4cd49f9a22f2bb0d4614
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-28 16:44:10 +0900

    feat: setQueryData함수로 쿼리키에 해당하는 데이터 업데이트 하기

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -29,6 +29,10 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
+  // useQueryClient를 호출하면 위에서 생성자 함수,
+  // QueryClient를 통해 만들어진 객체의 정보를 얻을 수 있다.
+  const queryClient = useQueryClient();
+
   // TODO: call useQuery to update user data from server
   const { data: user } = useQuery(queryKeys.user, () => getUser(user));
   // 왼쪽 user와 오른쪽 user는 같은 변수이다. 기존의 user를 대입해서 새로운 userData를 불러오는것.
@@ -36,11 +40,15 @@ export function useUser(): UseUser {
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
+    // setQueryData로 키값에 해당하는 데이터를 업데이트한다.
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    queryClient.setQueryData(queryKeys.user, null);
+    // 해당 키 값에 해당하는 데이터를 null로 저장시킨다.
   }
 
   return { user, updateUser, clearUser };
