b91429eb37c4ae983f591e8011e8182f0664ea3a
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-30 02:06:32 +0900

    refactor: 주석 추가

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -37,6 +37,7 @@ export function useUser(): UseUser {
   const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
     initialData: getStoredUser, // 쿼리키에 해당 함수의 return 값을 초기데이터로 넣는다.
     // 즉 user 데이터가 localStorage에 잇으면, 해당 데이터를 캐시에 저장시켜 서버통신해서 가져온 데이터처럼 보이게한다.
+    // :::: 비추인게, userData를 sessionStorage에라도 넣어야하는데, 데이터를 storage에 넣는건 비추.
     onSuccess: (received: User | null) => {
       // 쿼리 함수가 성공적으로 실행됐을때 실행되는 함수, data를 파라미터로 받는다.
       if (!received) {
