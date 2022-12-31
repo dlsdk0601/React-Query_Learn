6cd2787fca41eeeaacd24b411423e814bc744b87
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-31 16:42:54 +0900

    feat: usePatchUser hook 설정

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,37 +1,56 @@
 import jsonpatch from 'fast-json-patch';
+import { UseMutateFunction, useMutation } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
 import { useUser } from './useUser';
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null,
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData),
+    },
+  );
+  return data.user;
+}
 
 // TODO: update type to UseMutateFunction type
-export function usePatchUser(): (newData: User | null) => void {
+export function usePatchUser(): UseMutateFunction<
+  User,
+  unknown,
+  User,
+  unknown
+> {
   const { user, updateUser } = useUser();
+  const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const { mutate: patchUser } = useMutation(
+    (newUserData: User) => patchUserOnServer(newUserData, user),
+    {
+      // onSuccess의 콜백함수는 queryFn함수의 return 값을 파라미터로 받는다.
+      onSuccess: (userData: User | null) => {
+        if (user) {
+          updateUser(userData);
+          toast({
+            title: 'User updated!',
+            status: 'success',
+          });
+        }
+      },
+    },
+  );
 
   return patchUser;
 }
