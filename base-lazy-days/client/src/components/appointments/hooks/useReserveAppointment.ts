c3e5071287465b5556b8d7905ae5fe9c242a84bf
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-31 15:54:45 +0900

    feat: useReserveAppointment hook에서 useMutation 처리 하고 return에 mutate 처리. type도 같이 처리

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,3 +1,5 @@
+import { UseMutateFunction, useMutation } from 'react-query';
+
 import { Appointment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -5,27 +7,38 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 import { useUser } from '../../user/hooks/useUser';
 
 // for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined,
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? 'replace' : 'add';
+  const patchData = [{ op: patchOp, path: '/userId', value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
-// TODO: update type for React Query mutate function
-type AppointmentMutationFunction = (appointment: Appointment) => void;
+// mutate 의 type은 UseMutateFunction으로 처리 할 수 있다.
+// UseMutateFunction<TData, TError, TVariable, TContext>
+// TData: mutation function 의 리턴되는 데이터 타입
+// TError: 에러 타입
+// TVariables: mutate function의 변수 타입
+// TContext: context type
 
-export function useReserveAppointment(): AppointmentMutationFunction {
+export function useReserveAppointment(): UseMutateFunction<
+  void,
+  unknown,
+  Appointment,
+  unknown
+> {
   const { user } = useUser();
   const toast = useCustomToast();
 
+  const { mutate } = useMutation((appointment: Appointment) =>
+    setAppointmentUser(appointment, user?.id),
+  );
+
   // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  return mutate;
 }
