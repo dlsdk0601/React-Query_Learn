945a4871559a5a81144e6a9120601beef161787d
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-01 22:52:41 +0900

    feat: component 테스트 말고, screen을 통해서 API 테스트도 생성.

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   // write test here
 
   // 아래처럼 테스트 하면 실패한다. QueryClient가 없다고,
@@ -12,4 +12,13 @@ test('renders response from query', () => {
 
   // 그래서 custom해서 가져온다.
   renderWithQueryClient(<Treatments />);
+
+  // heading은 해당 테스트의 제목
+  // option에 들어간 내용중 name은 API res에 name이라는 필드 찾아서, 해당 내용이 포함됐는지를 확인.
+  const treatmentTitles = await screen.findAllByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+
+  // true가 3개 나온건지 확인
+  expect(treatmentTitles).toHaveLength(3);
 });
