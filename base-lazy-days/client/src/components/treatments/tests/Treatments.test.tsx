9479caaa7f4da5073a7747f533c092f75962fd7f
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-01 22:42:08 +0900

    feat: queryClient 로 테스트가 안되서, custom render 함수 만든거 들고와서 테스트

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,15 @@
 import { screen } from '@testing-library/react';
 
+import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
 test('renders response from query', () => {
   // write test here
+
+  // 아래처럼 테스트 하면 실패한다. QueryClient가 없다고,
+  // Treatments라는 컴포넌트 단독으로 테스트하기 때문에,
+  // render(<Treatments />);
+
+  // 그래서 custom해서 가져온다.
+  renderWithQueryClient(<Treatments />);
 });
