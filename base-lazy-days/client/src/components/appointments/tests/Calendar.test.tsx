0c1ebbfe473242b044f566266511e756d5368505
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-01 23:32:43 +0900

    feat: 캘린더 컴포넌트에 error 테스트 코드 작성

--- base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/Calendar.test.tsx
@@ -1,9 +1,9 @@
 import { screen } from '@testing-library/react';
+import { rest } from 'msw';
 
-// import { rest } from 'msw';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { renderWithClient } from '../../../test-utils';
+import { renderWithQueryClient } from '../../../test-utils';
 import { Calendar } from '../Calendar';
 
 // mocking useUser to mimic a logged-in user
@@ -12,14 +12,21 @@ import { Calendar } from '../Calendar';
 //   useUser: () => ({ user: mockUser }),
 // }));
 
-test('Reserve appointment error', () => {
+test('Reserve appointment error', async () => {
   // (re)set handler to return a 500 error for appointments
-  // server.resetHandlers(
-  //   rest.get(
-  //     'http://localhost:3030/appointments/:month/:year',
-  //     (req, res, ctx) => {
-  //       return res(ctx.status(500));
-  //     },
-  //   ),
-  // );
+  server.resetHandlers(
+    rest.get(
+      'http://localhost:3030/appointments/:month/:year',
+      (req, res, ctx) => {
+        return res(ctx.status(500));
+      },
+    ),
+  );
+
+  renderWithQueryClient(<Calendar />);
+
+  // 달력 테스트 경우, error 발생시키고, 해당 toast모달창 잘뜨는지 테스트
+  const alertToast = await screen.findByRole('alter');
+
+  expect(alertToast).toHaveTextContent('Request failed with status code 500');
 });
