23780633367d4e284b760158d01f80ba7061840c
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-02 23:14:23 +0900

    refactor: useAppointment 테스트 하기

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,8 +1,27 @@
-// import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
-// import { createWrapper } from '../../../test-utils';
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
 
 test('filter appointments by availability', async () => {
-  // test goes here
+  // result는 hook의 결과를 포함한 객체
+  // wairFor는 hook의 비동기 액션을 기다릴수 있게 하는 역할
+  const { result, waitFor } = renderHook(useAppointments, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // appointments가 업데이트 되길 기다리기
+  // result.current useAppointment가 반환한 객체
+  await waitFor(() => Object.keys(result.current.appointments).length > 0);
+
+  const filteredAppointmentsLength = Object.keys(result.current.appointments)
+    .length;
+
+  act(() => result.current.setShowAll(true));
+
+  await waitFor(
+    () =>
+      Object.keys(result.current.appointments).length >
+      filteredAppointmentsLength,
+  );
 });
