21c2d284234c0aa9887698020b8ae620f3b079e4
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-02 22:25:12 +0900

    feat: AllStaff에 대한 error 테스트 코드 작성

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,8 +1,9 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { rest } from 'msw';
+import { QueryClientProvider, setLogger } from 'react-query';
 
 import { server } from '../../../mocks/server';
-import { defaultQueryClientOptions } from '../../../react-query/queryClient';
+import { generateQueryClient } from '../../../react-query/queryClient';
 import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -18,9 +19,31 @@ test('renders response from query', async () => {
 
 test('handles query error', async () => {
   // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.resetHandlers(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+  );
+
+  setLogger({
+    log: console.log,
+    warn: console.warn,
+    error: () => {
+      // swallow errors without printing out
+    },
+  });
+
+  const queryClient = generateQueryClient();
+  const options = queryClient.getDefaultOptions();
+  options.queries = { ...options.queries, retry: false };
+  queryClient.setDefaultOptions(options);
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <AllStaff />
+    </QueryClientProvider>,
+  );
+
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('Request failed with status code 500');
 });
