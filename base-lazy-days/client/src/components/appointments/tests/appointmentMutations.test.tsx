dc15e5e92f7e7daf623a8bc59a12df376047ad58
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-02 22:35:24 +0900

    feat: 예약 mutate에 대한 API 테스트 코드 작성

--- base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -6,8 +6,11 @@ import {
 import { MemoryRouter } from 'react-router-dom';
 
 import { mockUser } from '../../../mocks/mockData';
-// import { renderWithQueryClient } from '../../../test-utils';
+import { renderWithQueryClient } from '../../../test-utils';
 import { Calendar } from '../Calendar';
+
+// mutate에 대한 테스트 코트는 성공 토스트 모달을 잘 받아오냐로 판별한다.
+// 사실 실제로 데이터가 바뀌는지 확인해야하지만, 강의에서는 거기까지 알려주지않는다.
 
 // mocking useUser to mimic a logged-in user
 jest.mock('../../user/hooks/useUser', () => ({
@@ -16,22 +19,29 @@ jest.mock('../../user/hooks/useUser', () => ({
 }));
 
 test('Reserve appointment', async () => {
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+
   // find all the appointments
-  // const appointments = await screen.findAllByRole('button', {
-  //   name: /\d\d? [ap]m\s+(scrub|facial|massage)/i,
-  // });
-  //
-  // // click on the first one to reserve
-  // fireEvent.click(appointments[0]);
-  //
-  // // check for the toast alert
-  // const alertToast = await screen.findByRole('alert');
-  // expect(alertToast).toHaveTextContent('reserve');
-  //
-  // // close alert to keep state clean and wait for it to disappear
-  // const alertCloseButton = screen.getByRole('button', { name: 'Close' });
-  // alertCloseButton.click();
-  // await waitForElementToBeRemoved(alertToast);
+  // screen은 화면단에서 찾는거임 아마 html로 찾을듯
+  const appointments = await screen.findAllByRole('button', {
+    name: /\d\d? [ap]m\s+(scrub|facial|massage)/i,
+  });
+
+  // click on the first one to reserve
+  fireEvent.click(appointments[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('reserve');
+
+  // close alert to keep state clean and wait for it to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'Close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
 });
 
 test('Cancel appointment', async () => {
